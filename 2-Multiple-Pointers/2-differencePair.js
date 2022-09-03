1b9bb0106180b40dc1d20702d9f1fd0a5781943e
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2022-09-03 15:33:15 +0530

    03-09-22

--- 2-Multiple-Pointers/2-differencePair.js
+++
@@ -1,8 +0,0 @@
-/**
- * Javascript Program to Find a pair with the given difference
- * Input: arr[] = {5, 20, 3, 2, 50, 80}, n = 78
-Output: Pair Found: (2, 80)
-
-Input: arr[] = {90, 70, 20, 80, 50}, n = 45
-Output: No Such Pair
- */
