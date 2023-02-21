cc344684e5c2d641808d9fae9f9a2424b0180c7c
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2023-02-21 23:48:46 +0530

    array quest added

---
+++ Arrays/1-union-of-2-arrays.js
@@ -0,0 +1,10 @@
+//print the union of an array with no duplicates
+function arrayUnion(arr1, arr2) {
+  return [...arr1, ...arr2];
+}
+
+console.log(arrayUnion([1, 3, 5, 7, 9], [2, 4, 6, 8]));
+
+//variations
+//1- sorted outpur
+//2- duplicates exists
