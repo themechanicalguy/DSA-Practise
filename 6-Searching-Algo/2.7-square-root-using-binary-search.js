fbb10c3795eab1421080b875ceb80fc734615b89
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2023-02-21 08:40:27 +0530

    binary search added

--- 6-Searching-Algo/2.7-square-root-using-binary-search.js
+++ 6-Searching-Algo/2.7-square-root-using-binary-search.js
@@ -1,1 +1,22 @@
- 
+//find the square root of a number using binary search
+function sqrt(num) {
+  let start = 0;
+  let end = num;
+  let target = num;
+  let ans = -1;
+  while (start <= end) {
+    let mid = Math.floor(start + end - start / 2);
+    if (mid * mid === target) {
+      return mid;
+    }
+    if (mid * mid > target) {
+      end = mid - 1;
+    } else {
+      ans = mid;
+      start = mid + 1;
+    }
+  }
+  return ans;
+}
+
+console.log(sqrt(12));
