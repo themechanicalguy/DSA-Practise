6454af9ea9ac2630ce0c8f6a93477748d670f9e7
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2023-03-12 16:59:38 +0530

    old codes commit

--- 6-Searching-Algo/2.5-find-pivot-using-binary-search.js
+++ 6-Searching-Algo/2.5-find-pivot-using-binary-search.js
@@ -12,35 +12,31 @@
 function pivot(arr) {
   let start = 0;
   let end = arr.length - 1;
-  while (start < end) {
-    let mid = Math.floor(start + (end - start) / 2);
+  let mid = Math.floor(start + (end - start) / 2);
+  while (start <= end) {
+    if (start === end) {
+      return start;
+    }
     //suppose our mid comes at index 4, then arr[mid] == 7
     // 7 is greater then both sides
     // so this is the pivot, we need to handle it seperately
     if (mid + 1 < arr.length && arr[mid] > arr[mid + 1]) {
-      return arr[mid];
+      return mid;
     }
     //suppose out mid comes at index 5, then arr[mid] == 1,
     // 1 is smaller then both side
     // so pivot is mid-1, we need to handle it seperately
     if (mid - 1 >= 0 && arr[mid - 1] > arr[mid]) {
-      return arr[mid - 1];
+      return mid - 1;
     }
-    // bad practise
-    // if (arr[mid] > arr[start]) {
-    //   start = mid;
-    // } else if (arr[mid] < arr[start]) {
-    //   end = mid - 1;
-    // }
-
     if (arr[start] >= arr[mid]) {
       end = mid - 1;
     } else {
-      //for single element make sure using start < end in while loop
-      start = mid;
+      start = mid + 1;
     }
+    mid = Math.floor(start + (end - start) / 2);
   }
-  return start;
+  return -1;
 }
 
-console.log(pivot([[4, 5, 6, 7, 0, 1, 2]]));
+console.log(pivot([5, 6, 7, 8, 9, 10, 1, 2, 3]));
