c2955bba80cc022f3e4973ce9d21d33166ea77be
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2023-04-12 00:48:55 +0530

    Singly LL

---
+++ 8-Linked-List-singly/singlyLinkedList.js
@@ -0,0 +1,30 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    // Create a new node using value passed to the function
+    var newNode = new Node(val);
+    // if no head
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+let list = new SinglyLinkedList();
+list.push(10);
