65ec31f2d9d9c58f958ab39735f5e3df4191b993
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2023-04-13 11:49:44 +0530

    rat in a maze revision

--- 4-Divide-and-conquer-bc/5-Rat-in-a-maze.js
+++ 4-Divide-and-conquer-bc/5-Rat-in-a-maze.js
@@ -1,28 +1,35 @@
 function isSafe(x, y, visited, m, n) {
-  if (
+  return !!(
     x >= 0 &&
     x < n &&
     y >= 0 &&
     y < n &&
     visited[x][y] === 0 &&
     m[x][y] === 1
-  ) {
-    return true;
-  } else {
-    return false;
-  }
+  );
+
+  // if (
+  //   x >= 0 &&
+  //   x < n &&
+  //   y >= 0 &&
+  //   y < n &&
+  //   visited[x][y] === 0 &&
+  //   m[x][y] === 1
+  // ) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
 }
 
 function solve(m, n, ans, x, y, visited, path) {
   //base case
   if (x === n - 1 && y === n - 1) {
-    console.log(path, "path");
-    if (path !== undefined) {
-      ans.push(path);
-    }
+    ans.push(path);
     return;
   }
   visited[x][y] = 1;
+
   //down
   let newX = x + 1;
   let newY = y;
@@ -64,12 +71,18 @@ function solve(m, n, ans, x, y, visited, path) {
 }
 
 function findPath(m, n) {
+  // initialize an array to store result
   let ans = [];
+  // if origin is 0 return ans(empty result)
   if (m[0][0] === 0) return ans;
+  // Starting index to check path
   let srcX = 0;
   let srcY = 0;
+  // Clone m matrix to create a visited array
   const visited = structuredClone(m);
+  // initially fill the entire matrix with 0
   visited.forEach((item) => item.fill(0));
+  // to store path
   let path = "";
   let res = solve(m, n, ans, srcX, srcY, visited, path);
   return res.sort();
