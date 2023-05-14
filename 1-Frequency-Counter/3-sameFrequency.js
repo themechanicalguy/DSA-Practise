4194e505f4dc69e74d057d2d7d8cab9f7c0488f5
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2023-05-14 23:24:12 +0530

    new quest added

--- 1-Frequency-Counter/3-sameFrequency.js
+++ 1-Frequency-Counter/3-sameFrequency.js
@@ -1,3 +1,32 @@
+// Frequency Counter - sameFrequency
+// Write a function called sameFrequency. Given two positive integers,
+// find out if the two numbers have the same frequency of digits.
+
+// Your solution MUST have the following complexities: Time: O(N)
+
+function sameFrequency(num1, num2) {
+  const str1 = `${num1}`;
+  const str2 = `${num2}`;
+
+  if (str1.length !== str2.length) return false;
+
+  const obj = {};
+
+  for (const char of str1) {
+    obj[char] = ++obj[char] || 1;
+  }
+
+  for (const char of str2) {
+    if (!obj[char]) return false;
+    obj[char]--;
+  }
+
+  return true;
+}
+
+console.log(sameFrequency(34, 14)); // false
+console.log(sameFrequency(3589578, 5879385)); // true
+
 /**
  * Check if a string has all characters with same frequency with one variation allowed
  */
