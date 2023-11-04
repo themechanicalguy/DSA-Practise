c85e94e3991679e07f3d5816e601d7fa62a1f7c8
Saurav Rath <83498199+themechanicalguy@users.noreply.github.com>
2023-11-05 03:04:53 +0530

    updated

--- 7-Sorting/5-quick-sort.js
+++ 7-Sorting/5-quick-sort.js
@@ -3,16 +3,12 @@
 //recurssion call
 
 //swap function
-function swap(arr, i, j) {
-  [arr[i], arr[j]] = [arr[j], arr[i]];
-}
-
 //find the pivot index
 function pivot(arr, start, end) {
   let pivotItem = arr[start];
   let swapIndex = start;
 
-  for (let i = start + 1; i < arr.length; i++) {
+  for (let i = start + 1; i <= end; i++) {
     if (arr[i] < pivotItem) {
       swapIndex++;
       swap(arr, swapIndex, i);
@@ -22,16 +18,25 @@ function pivot(arr, start, end) {
   return swapIndex;
 }
 
+//swap function
+//swap function
+function swap(arr, i, j) {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+}
+
 function quickSort(arr, start = 0, end = arr.length - 1) {
-  let pivotIndex = pivot(arr, start, end);
   //base case
-  if (start >= end) return arr;
-  //left partition sort
-  quickSort(arr, start, pivotIndex - 1);
-  //right partition
-  quickSort(arr, pivotIndex + 1, end - 1);
+  if (start < end) {
+    let pivotIndex = pivot(arr, start, end);
+    //left partition sort
+    quickSort(arr, start, pivotIndex - 1);
+    //right partition
+    quickSort(arr, pivotIndex + 1, end);
+  }
   return arr;
 }
-let arr = [8, 20, 4, 0, 2, 50, 1];
+let arr = [80, 10, 40, 0.3, 0.2, 20, 50, 30];
+
+quickSort(arr);
 
 quickSort(arr);
