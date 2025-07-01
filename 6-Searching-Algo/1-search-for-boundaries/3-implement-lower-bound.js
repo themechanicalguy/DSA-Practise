9d959ab72be64b78741bb156f36fdf4b33bc94b9
Venkataramanan <venkatbala214@gmail.com>
2025-07-01 05:33:59 +0530

    Lower Bound

--- 6-Searching-Algo/1-search-for-boundaries/3-implement-lower-bound.js
+++ 6-Searching-Algo/1-search-for-boundaries/3-implement-lower-bound.js
@@ -91,3 +91,30 @@ testValues.forEach((x) => {
   console.log("Built-in Method:", findFloorBuiltIn(testArray, x));
   console.log("---");
 });
+
+
+
+
+/** Below is solved by Author: Venkataramanan */
+
+function findFloorLinear (arr, x) {
+  for (let i=0; i<arr.length;i++) {
+    if (arr[i] >= x) return i;
+  }
+  return arr.length;
+
+}
+
+function findFloorBinary (arr, x) {
+  let left =0, right=arr.length-1, ans=-1;
+  while (left <=right) {
+    const mid = Math.floor((left+right) / 2);
+    if (arr[mid] >= x) {
+      right = mid-1;
+      ans = mid;
+    } else {
+      left = mid-1;
+    }
+  }
+  return ans;
+}
