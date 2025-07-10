5da49acca52196fe956c270645a1936d2efeeec0
Venkataramanan <venkatbala214@gmail.com>
2025-07-10 06:43:01 +0530

    Rotated Sorted Array

--- 6-Searching-Algo/2-search-in-rotated-sorted-arr/10-find-min-in-rotated-sorted-arr.js
+++ 6-Searching-Algo/2-search-in-rotated-sorted-arr/10-find-min-in-rotated-sorted-arr.js
@@ -29,3 +29,27 @@ var findMin = function (nums) {
 
   return nums[left];
 };
+
+// author: Venkataramanan, Leetcode Submitted Anser by me
+var search = function(nums, target) {
+  let left=0, right=nums.length-1;
+  while (left <= right) {
+      const mid = Math.floor((left + right)/2);
+      if ( nums[mid] === target ) {
+          return mid;
+      } else if( nums[left] <= nums[mid]) {
+          if (nums[left] <= target && target <= nums[mid]) {
+              right = mid - 1;
+          } else {
+              left = mid + 1;
+          }
+      } else {
+          if (nums[mid] <= target && target <= nums[right]) {
+              left = mid + 1;
+          } else {
+              right = mid - 1;
+          }
+      }
+  }
+  return -1;
+};
